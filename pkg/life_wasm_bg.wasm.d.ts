e31a974926cfc3ba29f0b5ed83d4a287111c3c91
rahulc29 <rahul29112002@gmail.com>
2021-11-21 16:22:12 +0530

    Add `pkg` to git

---
+++ pkg/life_wasm_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_universe_free(a: number): void;
+export function universe_toggle(a: number, b: number, c: number): void;
+export function universe_grid(a: number): number;
+export function universe_get(a: number, b: number, c: number): number;
+export function universe_height(a: number): number;
+export function universe_width(a: number): number;
+export function universe_render(a: number, b: number): void;
+export function universe_delta(a: number): number;
+export function universe_new(): number;
+export function universe_update(a: number): void;
+export function greet(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
